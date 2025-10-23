1ea654c3ce05e241045332e92585a8fd8d18f06c
ngapham <nga.pham.cs83@gmail.com>
2025-10-23 11:23:01 +0700

    fix conflicts

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import {Button} from 'react-bootstrap';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
