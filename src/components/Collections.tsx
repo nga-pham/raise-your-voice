1bac5077475107af2e242703c49622346e24b55b
ngapham <nga.pham.cs83@gmail.com>
2025-10-23 18:57:07 +0700

    First style for collection

---
+++ src/components/Collections.tsx
@@ -0,0 +1,48 @@
+﻿import { Container, Row, Col, Card } from "react-bootstrap";
+import jsonData from "../assets/you-are-not-alone.json";
+
+const lang = "vn"; // "en" or "vn"
+
+
+const Collections = () => {
+    return (
+        <>
+            {/* You are not alone */}
+            <section className="py-5 mt-2 ">
+                <Container>
+                    <Row className="text-center justify-content-center p-3">
+                        <h2 className="mb-4" style={{fontWeight: 'bold'}}>Bạn không cô đơn</h2>
+                        <p className="mb-4" style={{fontSize: '1.5rem'}}>Cùng lắng nghe người nổi tiếng chia sẻ về lạm dụng tình dục</p>
+                        {jsonData.map(item => {
+                            if (item.lang.trim() === lang.trim()) {
+                            return <Row>
+                                {item.collection.map((collection, _idx) => {
+                                    return (
+                                        <Col md={4}>
+                                            <Card key={_idx} className="m-2" style={{ minWidth: '18rem' }}>
+                                        <Card.Img variant="top" src={collection.photo} />
+                                        <Card.Body>
+                                            <Card.Title>
+                                            <p style={{ color: '#000', textAlign: 'left' }}>{collection.title}</p>
+                                            </Card.Title>
+                                            <Card.Text style={{ color: '#000', textAlign: 'left', fontSize: '1rem' }}>
+                                                <p>{collection.description}</p>
+                                            </Card.Text>
+
+                                        </Card.Body>
+                                    </Card>
+                                        </Col>
+                                    )
+                                })}
+                            </Row>
+                            }
+                        })}
+                    </Row>
+                </Container>
+            </section>
+        </>
+    );
+};
+
+export default Collections;
+
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Collections from "../components/Collections";
 
 const Index = () => {
     return (
         <div className="min-h-screen">
             <Header />
+
+            <Collections />
+
             <Footer />
         </div>
     );
