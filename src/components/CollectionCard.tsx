9bb0bd0b76786eb36463d45ec30626a8ce3ca9a2
ngapham <nga.pham.cs83@gmail.com>
2025-10-24 11:04:32 +0700

    add new section in collection

---
+++ src/components/CollectionCard.tsx
@@ -0,0 +1,31 @@
+﻿import { Card, Col } from "react-bootstrap";
+
+interface CollectionCardProps {
+    key: number | undefined
+    title: string | "";
+    photo: string | "";
+    description: string | "";
+    references: string | "";
+    caption: string | "";
+    }
+
+const CollectionCard = ({key, title, photo, description, references, caption}: CollectionCardProps) => {
+    return (
+        <Col md={4} >
+            <Card className="m-2" style={{ minWidth: '18rem' }}>
+                <Card.Img variant="top" src={photo} />
+                <Card.Body>
+                    <Card.Title>
+                        <p style={{ color: '#000', textAlign: 'left' }}>{title}</p>
+                    </Card.Title>
+                    <Card.Text style={{ color: '#000', textAlign: 'left', fontSize: '1rem' }}>
+                        {description}
+                    </Card.Text>
+                    <Card.Link href={references}>{caption}</Card.Link>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
+
+export default CollectionCard
--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -1,40 +1,61 @@
-﻿import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import jsonData from "../assets/you-are-not-alone.json";
-import { useNavigate } from 'react-router-dom';
+﻿import { Container, Row } from "react-bootstrap";
+import jsonSection1 from "../assets/key-facts.json"
+import jsonSection2 from "../assets/you-are-not-alone.json";
+import CollectionCard from "../components/CollectionCard"
 
 const lang = "vn"; // "en" or "vn"
 
 const Collections = () => {
 
-
     return (
         <>
+            {/* Key facts */}
+            <section className="py-5 mt-2">
+                <Container>
+                    <Row className="text-center justify-content-center p-3">
+                        <h2 className="mb-4" style={{ fontWeight: 'bold' }}>Thông tin cơ bản</h2>
+                        <p className="mb-4" style={{ fontSize: '1.5rem' }}>Cùng tìm hiểu về quấy rối, bạo lực và xâm hại tình dục và các vấn đề liên quan</p>
+                        {jsonSection1.map(item => {
+                            if (item.lang.trim() === lang.trim()) {
+                                return <Row>
+                                    {item.collection.map((collection, _idx) => {
+                                        return (
+                                            <CollectionCard
+                                                key={_idx}
+                                                title={collection.title}
+                                                photo={collection.photo}
+                                                description={collection.description}
+                                                references={collection.references}
+                                                caption={collection.caption}
+                                            />
+                                        )
+                                    })}
+                                </Row>
+                            }
+                        })}
+                    </Row>
+                </Container>
+            </section>
+
             {/* You are not alone */}
             <section className="py-5 mt-2">
                 <Container>
                     <Row className="text-center justify-content-center p-3">
                         <h2 className="mb-4" style={{fontWeight: 'bold'}}>Bạn không cô đơn</h2>
                         <p className="mb-4" style={{fontSize: '1.5rem'}}>Cùng lắng nghe người nổi tiếng chia sẻ về lạm dụng tình dục</p>
-                        {jsonData.map(item => {
+                        {jsonSection2.map(item => {
                             if (item.lang.trim() === lang.trim()) {
                             return <Row>
                                 {item.collection.map((collection, _idx) => {
                                     return (
-                                        <Col md={4} key={_idx} >
-                                            <Card className="m-2" style={{ minWidth: '18rem' }}>
-                                        <Card.Img variant="top" src={collection.photo} />
-                                        <Card.Body>
-                                            <Card.Title>
-                                            <p style={{ color: '#000', textAlign: 'left' }}>{collection.title}</p>
-                                            </Card.Title>
-                                            <Card.Text style={{ color: '#000', textAlign: 'left', fontSize: '1rem' }}>
-                                                {collection.description}
-                                            </Card.Text>
-                                            <Card.Link href={collection.references}>{collection.caption}</Card.Link>
-                                        </Card.Body>
-                                            
-                                    </Card>
-                                        </Col>
+                                        <CollectionCard
+                                            key={_idx}
+                                            title={collection.title}
+                                            photo={collection.photo}
+                                            description={collection.description}
+                                            references={collection.references}
+                                            caption={collection.caption}
+                                        />
                                     )
                                 })}
                             </Row>
