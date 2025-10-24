000f36683b8a54369d4194a210caa010f9a8ad40
ngapham <nga.pham.cs83@gmail.com>
2025-10-24 15:01:38 +0700

    refine data

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -1,10 +1,8 @@
 ﻿import { Container, Row } from "react-bootstrap";
-import jsonSection1 from "../assets/key-facts.json"
-import jsonSection2 from "../assets/self-defense.json"
-import jsonSection3 from "../assets/you-are-not-alone.json";
+import vietnameseData from "../assets/vi.json"
 import CollectionCard from "../components/CollectionCard"
 
-const lang = "vn"; // "en" or "vn"
+const lang = "vi"; // "en" or "vn"
 
 const Collections = () => {
 
@@ -16,58 +14,30 @@ const Collections = () => {
                     <Row className="text-center justify-content-center p-3">
                         <h2 className="mb-4" style={{ fontWeight: 'bold' }}>Thông tin cơ bản</h2>
                         <p className="mb-4" style={{ fontSize: '1.5rem' }}>Cùng tìm hiểu về quấy rối, bạo lực và xâm hại tình dục và các vấn đề liên quan</p>
-                        {jsonSection1.map(item => {
-                            if (item.lang.trim() === lang.trim()) {
-                                return <Row>
-                                    {item.collection.map((collection, _idx) => {
-                                        return (
-                                            <CollectionCard
-                                                key={_idx}
-                                                title={collection.title}
-                                                photo={collection.photo}
-                                                description={collection.description}
-                                                references={collection.references}
-                                                caption={collection.caption}
-                                            />
-                                        )
-                                    })}
-                                </Row>
-                            }
-                        })}
+                        {vietnameseData.lang.trim() === lang.trim() ?
+                            <Row>
+                                {vietnameseData.collectionKeyFacts.map((collection, _idx) => {
+                                return (
+                                    <CollectionCard
+                                        key={_idx}
+                                        title={collection.title}
+                                        photo={collection.photo}
+                                        description={collection.description}
+                                        references={collection.references}
+                                        caption={collection.caption}
+                                    />
+                                )
+                            })}
+                        </Row> : null }
                     </Row>
 
             {/* Self-defense */}
                     <Row className="text-center justify-content-center p-3">
                         <h2 className="mb-4" style={{ fontWeight: 'bold' }}>Cách đối phó và tự vệ</h2>
                         <p className="mb-4" style={{ fontSize: '1.5rem' }}>Thực hành việc tự bảo vệ bản thân và hàn gắn trước hành vi quấy rối và xâm hại tình dục</p>
-                        {jsonSection2.map(item => {
-                            if (item.lang.trim() === lang.trim()) {
-                                return <Row>
-                                    {item.collection.map((collection, _idx) => {
-                                        return (
-                                            <CollectionCard
-                                                key={_idx}
-                                                title={collection.title}
-                                                photo={collection.photo}
-                                                description={collection.description}
-                                                references={collection.references}
-                                                caption={collection.caption}
-                                            />
-                                        )
-                                    })}
-                                </Row>
-                            }
-                        })}
-                    </Row>
-
-            {/* You are not alone */}
-                    <Row className="text-center justify-content-center p-3">
-                        <h2 className="mb-4" style={{fontWeight: 'bold'}}>Bạn không cô đơn</h2>
-                        <p className="mb-4" style={{fontSize: '1.5rem'}}>Cùng lắng nghe người nổi tiếng chia sẻ về lạm dụng tình dục</p>
-                        {jsonSection3.map(item => {
-                            if (item.lang.trim() === lang.trim()) {
-                            return <Row>
-                                {item.collection.map((collection, _idx) => {
+                        {vietnameseData.lang.trim() === lang.trim() ?
+                            <Row>
+                                {vietnameseData.collectionSelfDefense.map((collection, _idx) => {
                                     return (
                                         <CollectionCard
                                             key={_idx}
@@ -79,9 +49,28 @@ const Collections = () => {
                                         />
                                     )
                                 })}
-                            </Row>
-                            }
-                        })}
+                            </Row> : null}
+                    </Row>
+
+            {/* You are not alone */}
+                    <Row className="text-center justify-content-center p-3">
+                        <h2 className="mb-4" style={{fontWeight: 'bold'}}>Bạn không cô đơn</h2>
+                        <p className="mb-4" style={{fontSize: '1.5rem'}}>Cùng lắng nghe người nổi tiếng chia sẻ về lạm dụng tình dục</p>
+                        {vietnameseData.lang.trim() === lang.trim() ?
+                            <Row>
+                                {vietnameseData.collectionYouAreNotAlone.map((collection, _idx) => {
+                                    return (
+                                        <CollectionCard
+                                            key={_idx}
+                                            title={collection.title}
+                                            photo={collection.photo}
+                                            description={collection.description}
+                                            references={collection.references}
+                                            caption={collection.caption}
+                                        />
+                                    )
+                                })}
+                            </Row> : null}
                     </Row>
                 </Container>
             </section>
